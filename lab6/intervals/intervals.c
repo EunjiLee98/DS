40d6fde33288e19d46332bac35f50685e5e00119
EunjiLee98 <21700557@handong.edu>
2020-06-10 22:51:18 +0900

    Lab6

---
+++ lab6/intervals/intervals.c
@@ -0,0 +1,276 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct arraylist {
+	int capacity ;
+	int length ;
+	int unit ;
+	void * elements ;
+} ;
+
+typedef struct arraylist arraylist_t ;
+
+
+	arraylist_t * 
+arraylist_alloc (int unit) 
+{
+	arraylist_t * l ;
+
+	l = (arraylist_t *) malloc(sizeof(arraylist_t)) ;
+	l->capacity = 10 ;
+	l->length = 0 ;
+	l->unit = unit ;
+	l->elements = (int *) calloc(l->capacity, l->unit) ;
+	return l ;
+}
+
+	void
+arraylist_free (arraylist_t * l, void (* free_element)(void * e)) 
+{
+	if (free_element != 0x0) {
+		int i ;
+		for (i = 0 ; i < l->length ; i++) {
+			void * elem = l->elements + l->unit * i ;
+			free_element(elem) ;
+		}
+	}
+	free(l->elements) ;
+	free(l) ;
+}
+
+	int 
+arraylist_length (arraylist_t * l)
+{
+	return l->length ;
+}
+
+	void
+arraylist_print (arraylist_t * l, void (* print_element)(void * e)) 
+{
+	int i ;
+	for (i = 0 ; i < l->length ; i++) {
+		print_element(l->elements + l->unit * i) ;
+	}
+}
+
+	void
+arraylist_insert_first (arraylist_t * l, void * e)
+{
+	if (l->length == l->capacity) {
+		l->capacity *= 2 ;
+		l->elements = (int *) realloc(l->elements, (size_t) l->capacity * l->unit) ;
+	}
+	int i ;
+	for (i = l->length - 1 ; i >= 0 ; i--)
+		memcpy(l->elements + (i + 1) * l->unit, l->elements + i, l->unit) ;
+	memcpy(l->elements, e, l->unit) ;
+	l->length += 1 ;
+}
+
+	void
+arraylist_insert_last (arraylist_t * l, void * e)
+{
+	if (l->length == l->capacity) {
+		l->capacity *= 2 ;	
+		l->elements = (int *) realloc(l->elements, (size_t) l->capacity * l->unit ) ;
+	}
+	memcpy(l->elements + l->length * l->unit, e, l->unit) ;
+	l->length += 1 ;
+}
+
+	int
+arraylist_remove_first (arraylist_t * l, void * e) 
+{
+	if (l->length == 0)
+		return 1 ;
+
+	memcpy(e, l->elements, l->unit) ;
+	int i ;
+	for (i = 1 ; i < l->length ; i++) 
+		memcpy(l->elements + (i - 1) * l->unit, l->elements + i, l->unit) ;
+	l->length -= 1 ;
+	return 0 ;
+}
+
+	int
+arraylist_remove_last (arraylist_t * l, void * e) 
+{
+	if (l->length == 0)
+		return 1 ;
+
+	memcpy(e, l->elements + (l->length - 1) * l->unit, l->unit) ;
+	l->length -= 1 ;
+	return 0 ;
+}
+
+	int
+arraylist_get (arraylist_t * l, int pos, void * e)
+{
+	if (l->length <= pos) 
+		return 1 ;
+
+	memcpy(e, l->elements + pos * l->unit, l->unit) ;
+	return 0 ;
+}
+
+	void
+arraylist_sort (arraylist_t * l, int (* cmp_elements)(void * e1, void * e2))
+{
+	int i, j, m ;
+	char * t = malloc(l->unit) ;
+	for (i = 0 ; i < l->length - 1 ; i++) {
+		m = i ;
+		for (j = i + 1 ; j < l->length ; j++) {
+			if (cmp_elements(l->elements + j * l->unit, l->elements  + m * l->unit) < 0)
+				m = j ;
+		}
+		memcpy(t, l->elements + i * l->unit, l->unit) ;
+		memcpy(l->elements + i * l->unit, l->elements + m * l->unit, l->unit) ;
+		memcpy(l->elements + m * l->unit, t, l->unit) ;
+	}
+	free(t) ;
+}
+
+	void
+_arraylist_qsort (arraylist_t * l, int (* cmp_elements)(void * e1, void * e2), void * begin, void * end)
+{
+	if (begin >= end - l->unit)
+		return ;
+
+	int unit = l->unit ;
+	void * t = malloc(sizeof(unit)) ;
+
+	void * pivot = begin ;
+	void * lbound = begin + unit ;
+	void * rbound = end - unit ;
+
+	while (lbound <= rbound) {
+		while (lbound <= end - unit && cmp_elements(lbound, pivot) <= 0) 
+			lbound += unit ;
+
+		while (begin < rbound && cmp_elements(pivot, rbound) < 0) 
+			rbound -= unit ;
+
+		if (lbound <= rbound) {
+			memcpy(t, lbound, unit) ;
+			memcpy(lbound, rbound, unit) ;
+			memcpy(rbound, t, unit) ;
+			lbound += unit ;
+			rbound -= unit ;
+		}
+	}
+	memcpy(t, rbound, unit) ;
+	memcpy(rbound, pivot, unit) ;
+	memcpy(pivot, t, unit) ;
+	free(t) ;
+
+	if (rbound != end - unit)
+	{
+		_arraylist_qsort(l, cmp_elements, begin, rbound + unit) ;
+		_arraylist_qsort(l, cmp_elements, rbound + unit, end) ;
+	}
+	else
+	{
+		_arraylist_qsort(l,cmp_elements, begin, rbound);
+	}
+}
+
+	void
+arraylist_qsort (arraylist_t * l, int (* cmp_elements)(void * e1, void * e2))
+{
+	_arraylist_qsort(l, cmp_elements, l->elements, l->elements + l->length * l->unit) ;
+}
+
+void
+free_string (void * p)
+{ 
+		char ** s = (char **) p ;
+			free(*s) ;
+}
+
+	int
+cmp_double (void * e1, void * e2)
+{
+	double f1 = *((double *) e1) ;
+	double f2 = *((double *) e2) ;
+
+	if (f1 == 0x0 && f2 != 0x0)
+		return -1 ;
+
+	if (f1 != 0x0 && f2 == 0x0)
+		return 1 ;
+
+	return (f1 - f2) ;
+}
+
+	void
+print_double (void * p) 
+{
+	double f = *((double *) p) ;
+	printf("%lf\n", f) ;
+}
+
+	void
+print_string (void * p) 
+{
+	char * s = *((char **) p) ;
+	printf("%s\n", s) ;
+}
+
+	int 
+main () 
+{
+	arraylist_t * interval_vt, * interval_end_vt;
+	interval_vt = arraylist_alloc(sizeof(double)) ;
+	interval_end_vt = arraylist_alloc(sizeof(double)) ;
+
+	int n = 0;
+	int max = 0;
+	int count = 0;
+	double n1, n2;
+
+	scanf("%d",&n);
+
+	for(int i = 0; i < n; i++)
+	{
+		scanf("%lf %lf", &n1, &n2);	
+//		printf("%lf %lf\n", n1, n2);
+
+		arraylist_insert_last(interval_vt, &n1);
+		arraylist_insert_last(interval_vt, &n2);
+		arraylist_insert_last(interval_end_vt, &n2);
+	}
+
+	arraylist_qsort(interval_vt, cmp_double); 
+	arraylist_qsort(interval_end_vt, cmp_double);
+	
+	int i = 0;
+	int j = 0;
+	while(i < arraylist_length(interval_vt))
+	{
+		double data1, data2;
+		arraylist_get(interval_vt, i , &data1);
+		arraylist_get(interval_end_vt, j, &data2);
+
+		if(data1 != data2)
+		{
+			count += 1;
+
+			if(count > max)
+				max = count;
+		}
+		else if (data1 == data2)
+		{
+			count -= 1;
+			j++;
+		}
+		i++;
+	}
+	printf("%d\n",max);
+	//arraylist_print(interval_vt, print_double);
+	//printf("\n\n");
+	//arraylist_print(interval_end_vt, print_double);
+	
+	return 0;
+}
